9dc705d6db150c0bf3492a6d79eab4482cffbe02
Arnaldo Gualberto <arnaldo.g12@gmail.com>
2023-09-07 16:57:55 -0300

    test: crop

--- barbara_cv/cv/operations.py
+++ barbara_cv/cv/operations.py
@@ -17,16 +17,19 @@ def bright(image: NDArray[np.uint8], value: int) -> NDArray[np.uint8]:
 
 
 def crop(image: NDArray[np.uint8], top: int, left: int, bottom: int, right: int) -> NDArray[np.uint8]:
-    """Crop image.
+    """Crop an image
 
     Args:
-        image (np.ndarray): Image as numpy array.
-        top (int): Top border.
-        left (int): Left border.
-        bottom (int): Bottom border.
-        right (int): Right border.
+        image (NDArray[np.uint8]): 2D numpy array
+        top (int): y0
+        left (int): x0
+        bottom (int): y1
+        right (int): x1
 
     Returns:
-        np.ndarray: Image as numpy array.
+        NDArray[np.uint8]: image cropped by params.
     """
+    if top < 0 or left < 0 or bottom > image.shape[0] or right > image.shape[1]:
+        raise ValueError("Invalid crop parameters")
+
     return image[top:bottom, left:right]
